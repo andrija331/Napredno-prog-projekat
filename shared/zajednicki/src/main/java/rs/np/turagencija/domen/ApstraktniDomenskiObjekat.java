a796db0183f0c44c05f3f24261adcc6ba8b458a9
andrija331 <andrija.panovic333@gmail.com>
2025-10-15 15:05:55 +0200

    Dokumentovan apstraktni domenski objekat i klasa Klijent

--- shared/zajednicki/src/main/java/rs/np/turagencija/domen/ApstraktniDomenskiObjekat.java
+++ shared/zajednicki/src/main/java/rs/np/turagencija/domen/ApstraktniDomenskiObjekat.java
@@ -9,21 +9,66 @@ import java.util.List;
 import java.sql.*;
 
 /**
+ * Interfejs koji definise osnovne metode koje svaka klasa domena mora da
+ * implementira kako bi omogucila genericke CRUD operacije nad bazom podataka.
  *
- * @author KORISNIK
+ * <p>
+ * Interfejs se koristi kao apstrakcija između sloja baze podataka i domen
+ * modela. Omogućava različitim klasama da budu tretirane generički prilikom
+ * rada sa bazom, bez potrebe da se poznaje njihova konkretna
+ * implementacija.</p>
+ *
+ * @author Andrija Panovic
  */
 public interface ApstraktniDomenskiObjekat extends Serializable {
 
+    /**
+     * Vraća naziv tabele u bazi podataka koja odgovara konkretnom domenskom
+     * objektu.
+     *
+     * @return naziv tabele kao {@code String}
+     */
     public String vratiNazivTabele();
 
+    /**
+     * Na osnovu prosleđenog {@link ResultSet} objekta formira i vraća listu
+     * domenskih objekata.
+     *
+     * @param rs ResultSet koji sadrzi rezultate SQL upita
+     * @return lista domenskih objekata koji implementiraju ovaj interfejs
+     * @throws Exception ako dođe do greske prilikom citanja podataka iz
+     * ResultSet-a
+     */
     public List<ApstraktniDomenskiObjekat> vratiListu(ResultSet rs) throws Exception;
 
+    /**
+     * Vraca nazive kolona koje se koriste prilikom ubacivanja novog reda u
+     * bazu.
+     *
+     * @return niz naziva kolona odvojenih zarezima
+     */
     public String vratiKoloneZaUbacivanje();
 
+    /**
+     * Vraca vrednosti koje ce biti ubacene u bazu prilikom INSERT operacije.
+     *
+     * @return niz vrednosti kao {@code String}, formatiran za SQL upit
+     */
     public String vratiVrednostiZaUbacivanje();
 
+    /**
+     * Vraca primarni kljuc koji identifikuje konkretan zapis u bazi.
+     *
+     * @return primarni kljuc kao {@code String} u obliku "tabela.id=vrednost"
+     */
     public String vratiPrimarniKljuc();
 
+    /**
+     * Vraca deo SQL upita koji se koristi prilikom izmene (UPDATE) postojećeg
+     * reda.
+     *
+     * @return formatiran niz "kolona=vrednost" parova spremnih za SQL upit
+     */
     public String vratiVrednostZaIzmenu();
 
 }
--- shared/zajednicki/src/main/java/rs/np/turagencija/domen/Klijent.java
+++ shared/zajednicki/src/main/java/rs/np/turagencija/domen/Klijent.java
@@ -11,21 +11,61 @@ import java.util.Objects;
 import rs.np.turagencija.domen.ApstraktniDomenskiObjekat;
 
 /**
+ * Predstavlja klijenta turisticke agencije koji ima svoj jedinstveni ID, ime,
+ * prezime, email adresu i broj telefona.
  *
- * @author KORISNIK
+ * <p>
+ * Klasa implementira interfejs {@link ApstraktniDomenskiObjekat} i omogucava
+ * mapiranje objekta na odgovarajucu tabelu u bazi podataka ("klijent"). Koristi
+ * se u operacijama dodavanja, pretrage, azuriranja i brisanja podataka o
+ * klijentima.</p>
+ *
+ * @author Andrija Panovic
  */
-//public class Klijent implements ApstraktniDomenskiObjekat 
 public class Klijent implements ApstraktniDomenskiObjekat {
 
+    /**
+     * Jedinstveni identifikator klijenta u bazi podataka.
+     */
     private int klijentID;
+
+    /**
+     * Ime klijenta kao String.
+     */
     private String ime;
+
+    /**
+     * Prezime klijenta kao String.
+     */
     private String prezime;
+
+    /**
+     * Email adresa klijenta kao String.
+     */
     private String email;
+
+    /**
+     * Broj telefona klijenta kao Long.
+     */
     private long brojTelefona;
 
+    /**
+     * Podrazumevani konstruktor koji inicijalizuje objekat sa atributima koji
+     * imaju default vrednosti.
+     */
     public Klijent() {
     }
 
+    /**
+     * Konstruktor koji kreira objekat klase {@code Klijent} sa svim relevantnim
+     * podacima.
+     *
+     * @param klijentID jedinstveni identifikator klijenta
+     * @param ime ime klijenta
+     * @param prezime prezime klijenta
+     * @param email email adresa klijenta
+     * @param brojTelefona broj telefona klijenta
+     */
     public Klijent(int klijentID, String ime, String prezime, String email, long brojTelefona) {
         this.klijentID = klijentID;
         this.ime = ime;
@@ -34,57 +74,103 @@ public class Klijent implements ApstraktniDomenskiObjekat {
         this.brojTelefona = brojTelefona;
     }
 
+    /**
+     * Vraca tekstualni prikaz klijenta u formatu: ime, prezime i email.
+     *
+     * @return formatiran string sa osnovnim podacima o klijentu
+     */
     @Override
     public String toString() {
         return "ime=" + ime + ", prezime=" + prezime + ", email=" + email;
     }
 
+    /**
+     * @return jedinstveni identifikator klijenta
+     */
     public int getKlijentID() {
         return klijentID;
     }
 
+    /**
+     * @param KlijentID postavlja jedinstveni identifikator klijenta
+     */
     public void setKlijentID(int KlijentID) {
         this.klijentID = KlijentID;
     }
 
+    /**
+     * @return ime klijenta
+     */
     public String getIme() {
         return ime;
     }
 
+    /**
+     * @param ime postavlja ime klijenta
+     */
     public void setIme(String ime) {
         this.ime = ime;
     }
 
+    /**
+     * @return prezime klijenta
+     */
     public String getPrezime() {
         return prezime;
     }
 
+    /**
+     * @param prezime postavlja prezime klijenta
+     */
     public void setPrezime(String prezime) {
         this.prezime = prezime;
     }
 
+    /**
+     * @return email klijenta
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * @param email postavlja email klijenta
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * @return broj telefona klijenta
+     */
     public long getBrojTelefona() {
         return brojTelefona;
     }
 
+    /**
+     * @param brojTelefona postavlja broj telefona klijenta
+     */
     public void setBrojTelefona(long brojTelefona) {
         this.brojTelefona = brojTelefona;
     }
 
+    /**
+     * Racuna hash kod objekta na osnovu default vrednosti.
+     *
+     * @return hash vrednost objekta
+     */
     @Override
     public int hashCode() {
         int hash = 7;
         return hash;
     }
 
+    /**
+     * Poredi dva objekta klase {@code Klijent} na osnovu email adrese.
+     *
+     * @param obj objekat sa kojim se poredi
+     * @return {@code true} ako su email adrese iste, u suprotnom {@code false}
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
